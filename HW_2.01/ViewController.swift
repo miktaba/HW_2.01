2abd9e1c6d5fd49e9db532a5f86788e4322fe9fa
miktaba <mtabakaev@gmail.com>
2024-02-02 22:40:59 +0100

    Initial Commit

---
+++ HW_2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.01
+//
+//  Created by Mikhail Tabakaev on 2/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
